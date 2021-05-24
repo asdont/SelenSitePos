bdf114cbd3893c8240a6ef20a511336227573139
Dmitry <panshinda@yandex.ru>
2021-05-25 00:30:57 +0500

    distribution

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"fmt"
 	"github.com/tebeka/selenium"
 	"github.com/tebeka/selenium/chrome"
+	"strings"
 	"time"
 )
 
 func main() {
-	wd := initializationSelenium()
-	getPositions(wd)
+	pos, url := getPositions("labrika.ru", "лабрика инструменты для продвижения сайтов", "ru-ru")
+	fmt.Println(pos, url)
 }
 
-func initializationSelenium() selenium.WebDriver {
+func getPositions(domain string, keyword string, country string) (int, string) {
 	const (
 		seleniumPathChrome = `chromedriver` // ChromeDriver 91.0.4472.19
 		portChrome         = 9515
@@ -21,9 +22,9 @@ func initializationSelenium() selenium.WebDriver {
 	// Set the option of the selium service to null. Set as needed.
 	var ops []selenium.ServiceOption
 
-	service, err := selenium.NewChromeDriverService(seleniumPathChrome, portChrome, ops...)
-	if err != nil {
-		fmt.Printf("Error starting the ChromeDriver server: %v", err)
+	service, errNewDriver := selenium.NewChromeDriverService(seleniumPathChrome, portChrome, ops...)
+	if errNewDriver != nil {
+		fmt.Printf("Error starting the ChromeDriver server: %v", errNewDriver)
 	}
 
 	// Delay service shutdown
@@ -48,15 +49,18 @@ func initializationSelenium() selenium.WebDriver {
 			"--disable-notifications",
 			"--media-cache-size=1",
 			"--mute-audio",
+			// TODO менять на новых прокси
 			"--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7",
+			//"--user-agent=Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+
 		},
 	}
 
 	caps.AddChrome(chromeCaps)
 
-	wd, err := selenium.NewRemote(caps, "http://127.0.0.1:9515/wd/hub")
-	if err != nil {
-		panic(err)
+	wd, errNewRemote := selenium.NewRemote(caps, "http://127.0.0.1:9515/wd/hub")
+	if errNewRemote != nil {
+		panic(errNewRemote)
 	}
 
 	defer func(wd selenium.WebDriver) {
@@ -68,63 +72,80 @@ func initializationSelenium() selenium.WebDriver {
 	time.Sleep(10 * time.Second)
 	errWait := wd.SetImplicitWaitTimeout(3 * time.Second) // максимальное ожидание появления элемента
 	if errWait != nil {
-		fmt.Println(err)
+		fmt.Println(errWait)
 	}
 
-	errTimeout := wd.SetPageLoadTimeout(10 * time.Second) // тайм-аут для загрузки страницы
+	errTimeout := wd.SetPageLoadTimeout(1 * time.Second) // тайм-аут для загрузки страницы
 	if errTimeout != nil {
-		fmt.Println(err)
+		fmt.Println(errTimeout)
 	}
 
-	return wd
-}
-
-func getPositions(wd selenium.WebDriver) {
-	if err := wd.Get("https://duckduckgo.com/?q=кинотеатры+Санкт&kl=ru-ru"); err != nil {
-		panic(err)
+	// запрос страницы с требуемым keyword
+	targetKeyword := strings.Join(strings.Split(keyword, " "), "+")
+	targetUrl := fmt.Sprintf("https://duckduckgo.com/?q=%s&kl=%s", targetKeyword, country)
+	errGetUrl := wd.Get(targetUrl)
+	if errGetUrl != nil {
+		panic(errGetUrl)
 	}
-
-	time.Sleep(1 * time.Second)
 
 	for i := 0; i < 2; i++ {
-		// прокрутка страницы вниз
-		_, err := wd.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);", nil)
-		if err != nil {
-			fmt.Println(err)
+		// поиск целевых элементов(url-ы)
+		elements, errFindSelector := wd.FindElements(selenium.ByCSSSelector, "h2 .result__a")
+		if errFindSelector != nil {
+			fmt.Println(errFindSelector)
 		}
 
-		// ожидание от 3 до 5 секунд
+		// поиск требуемого домена
+		position, url := searchDomain(domain, elements)
+		if position != -1 {
+			return position, url
+		}
+
+		// если домен не найден
+		// прокрутка страницы вниз
+		_, errScript := wd.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);", nil)
+		if errScript != nil {
+			fmt.Println(errScript)
+		}
+
+		// ожидание от 2 до 5 секунд
 		randomNumber := time.Now().Nanosecond() % 10
-		if randomNumber < 3 || randomNumber > 5 {
+		if randomNumber < 2 || randomNumber > 5 {
 			randomNumber = 3
 			time.Sleep(time.Duration(randomNumber) * time.Second)
 		}
 
-		// клик по кнопке "больше результатов, если не подгрузилось"
+		// клик по кнопке "больше результатов, если результат не подгрузились скриптом"
 		next, _ := wd.FindElement(selenium.ByCSSSelector, ".result--more")
 		errClick := next.Click()
 		if errClick != nil {
-			fmt.Println(err)
+			fmt.Println(errClick)
 		}
 	}
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(60 * time.Second)
 
-	result, err := wd.FindElements(selenium.ByCSSSelector, "h2 .result__a")
-	if err != nil {
-		fmt.Println(err)
-	}
+	return -1, ""
+}
 
-	for position, domainTag := range result {
-		url, err := domainTag.GetAttribute("href")
-		if err != nil {
-			fmt.Printf("position %s - error -> %s", position, err)
+func searchDomain(domain string, elements []selenium.WebElement) (int, string) {
+	// поиск домена в урл-ах элементов
+	// TODO допилить поиск в только определённой порции
+	for position, tags := range elements {
+		url, errGetHref := tags.GetAttribute("href")
+
+		if errGetHref != nil {
+			fmt.Printf("position %s - error -> %s", position, errGetHref)
 		} else {
-			fmt.Printf("%d - %s\n", position, url)
+			if strings.Contains(domain, url) {
+				return position, url
+			}
 		}
+
+		fmt.Printf("%d - %s\n", position, url)
 	}
 
-	fmt.Println("\nполучено", len(result), "позиций")
+	fmt.Println("\nобработано", len(elements), "позиций")
 
-	time.Sleep(600 * time.Second)
+	return -1, ""
 }
